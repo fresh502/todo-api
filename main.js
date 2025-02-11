e9bd51f70c896417d4dc5d84ea7bfb7b63abb989
Sinrok Kim <fresh502@gmail.com>
2025-02-11 15:27:15 +0900

    add asyncHandler

--- main.js
+++ main.js
@@ -8,51 +8,84 @@ app.use(express.json());
 
 await mongoose.connect(DATABASE_URL);
 
-app.post("/tasks", async (req, res) => {
-  const data = req.body;
-  const newTask = await Task.create(data);
-  res.status(201).send(newTask);
-});
+function asyncHandler(handler) {
+  return async function (req, res) {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      if (e.name === "CastError") {
+        res.status(404).send({ message: "Cannot find given id" });
+      } else if (e.name === "ValidationError") {
+        res.status(400).send({ message: e.message });
+      } else {
+        res.status(500).send({ message: e.message });
+      }
+    }
+  };
+}
 
-app.get("/tasks", async (req, res) => {
-  // sort, count
-  const count = Number(req.query.count) || 0;
-  const sortOption =
-    req.query.sort === "oldest" ? ["createdAt", "asc"] : ["createdAt", "desc"];
-  const tasks = await Task.find().limit(count).sort([sortOption]);
-  res.send(tasks);
-});
-
-app.get("/tasks/:id", async (req, res) => {
-  const task = await Task.findById(req.params.id);
-  if (task) {
-    res.send(task);
-  } else {
-    res.status(404).send({ message: "Cannot find given id" });
-  }
-});
-
-app.patch("/tasks/:id", async (req, res) => {
-  const task = await Task.findById(req.params.id);
-  if (task) {
+app.post(
+  "/tasks",
+  asyncHandler(async (req, res) => {
     const data = req.body;
-    Object.keys(data).forEach((key) => {
-      task[key] = data[key];
-    });
-    await task.save();
-    res.send(task);
-  } else {
-    res.status(404).send({ message: "Cannot find given id" });
-  }
-});
+    const newTask = await Task.create(data);
+    res.status(201).send(newTask);
+  })
+);
 
-app.delete("/tasks/:id", async (req, res) => {
-  const task = await Task.findByIdAndDelete(req.params.id);
-  if (task) {
-    res.sendStatus(200);
-  } else {
-    res.status(404).send({ messsage: "Cannot find given id" });
-  }
-});
+app.get(
+  "/tasks",
+  asyncHandler(async (req, res) => {
+    // sort, count
+    const count = Number(req.query.count) || 0;
+    const sortOption =
+      req.query.sort === "oldest"
+        ? ["createdAt", "asc"]
+        : ["createdAt", "desc"];
+    const tasks = await Task.find().limit(count).sort([sortOption]);
+    res.send(tasks);
+  })
+);
+
+app.get(
+  "/tasks/:id",
+  asyncHandler(async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    if (task) {
+      res.send(task);
+    } else {
+      res.status(404).send({ message: "Cannot find given id" });
+    }
+  })
+);
+
+app.patch(
+  "/tasks/:id",
+  asyncHandler(async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    if (task) {
+      const data = req.body;
+      Object.keys(data).forEach((key) => {
+        task[key] = data[key];
+      });
+      await task.save();
+      res.send(task);
+    } else {
+      res.status(404).send({ message: "Cannot find given id" });
+    }
+  })
+);
+
+app.delete(
+  "/tasks/:id",
+  asyncHandler(async (req, res) => {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (task) {
+      res.sendStatus(200);
+    } else {
+      res.status(404).send({ messsage: "Cannot find given id" });
+    }
+  })
+);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
